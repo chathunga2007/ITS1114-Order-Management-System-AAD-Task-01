e65f80d844b99808e1529932a3cbf1f2c6b8656c
Chathunga Bimsara <wggachathungabimsara2007@gmail.com>
2026-07-19 10:37:24 +0530

    Spring Security Added!

--- src/main/java/lk/ijse/OrderManagementSystem/repository/UserRepository.java
+++ src/main/java/lk/ijse/OrderManagementSystem/repository/UserRepository.java
@@ -1,13 +1,14 @@
 package lk.ijse.OrderManagementSystem.repository;
 
-import lk.ijse.OrderManagementSystem.entity.User;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import java.util.List;
+import lk.ijse.OrderManagementSystem.entity.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
     @Query(value = "SELECT * FROM user WHERE role='CUSTOMER'", nativeQuery = true)
     List<User> getAllCustomer();
 
@@ -16,4 +17,8 @@ public interface UserRepository extends JpaRepository<User,Long> {
 
     @Query(value = "SELECT u FROM User u WHERE u.role='CUSTOMER'")
     List<User> getAllCustomerJPQL();
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByUsernameAndPassword(String username, String password);
 }
