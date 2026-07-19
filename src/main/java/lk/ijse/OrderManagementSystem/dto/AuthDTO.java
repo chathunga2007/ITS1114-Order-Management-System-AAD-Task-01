9263b61212375c74b1805ab76ba203d41deeddea
Chathunga Bimsara <wggachathungabimsara2007@gmail.com>
2026-07-19 10:35:34 +0530

    Spring Security Added!

---
+++ src/main/java/lk/ijse/OrderManagementSystem/dto/AuthDTO.java
@@ -0,0 +1,13 @@
+package lk.ijse.OrderManagementSystem.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthDTO {
+    private String userName;
+    private String password;
+}
